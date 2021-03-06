c414130b650c9262249f2a3cb1e4f4d7b796ab2b
Daniel <jdcornelio89@gmail.com>
2021-03-06 10:54:29 -0600

    first commit

---
+++ config/env.sh
@@ -0,0 +1,4 @@
+export NODE_ENV='production'
+export PORT=3000
+export SECRET='secret'
+export MONGO_URI='mongodb+srv://root:BiBjbFnNnYmma7fj@cluster0.j2yun.mongodb.net/users_database?retryWrites=true&w=majority'
